393659a1bab8f1327fbe4e7ab9014c6b5c2e7826
kolbeyang <kky2806@rit.edu>
2023-11-20 12:04:41 -0500

    New data analysis for genres and acoustic features

--- genres.py
+++ genres.py
@@ -20,3 +20,52 @@ def genres_per_artist(cursor):
     plt.ylabel("Number of artists")
     plt.title("Genres per artist")
     plt.show()
+
+def genres_audio_features(cursor):
+    # sql = """
+    #     SELECT bp_genre.genre_name FROM bp_track JOIN bp_genre ON bp_track.genre_id=bp_genre.genre_id GROUP BY bp_genre.genre_id ORDER BY COUNT(*) DESC LIMIT 15
+    # """
+    sql = """
+        SELECT 
+            bp_genre.genre_name AS genre,
+            AVG(audio_features.acousticness) AS avg_acousticness, 
+            AVG(audio_features.danceability) AS avg_danceability,
+            AVG(audio_features.energy) AS avg_energy,
+            AVG(audio_features.instrumentalness) AS avg_instrumentalness,
+            AVG(audio_features.liveness) AS avg_liveness
+        FROM bp_track 
+        INNER JOIN bp_genre ON bp_track.genre_id=bp_genre.genre_id
+        INNER JOIN audio_features ON bp_track.isrc=audio_features.isrc 
+        GROUP BY bp_track.genre_id
+        ORDER BY COUNT(bp_track.track_id) DESC
+        LIMIT 5
+    """
+
+    cursor.execute(sql)
+    result = cursor.fetchall()
+
+    for entry in result:
+        print(entry)
+
+    genres = [entry['genre'] for entry in result]
+    accousticness_values = [entry['avg_acousticness'] for entry in result]
+    danceability_values = [entry['avg_danceability'] for entry in result]
+    energy_values = [entry['avg_energy'] for entry in result]
+    instrumentalness_values = [entry['avg_instrumentalness'] for entry in result]
+    liveness_values = [entry['avg_liveness'] for entry in result]
+    
+    
+    X_axis = np.arange(len(genres)) 
+    
+    plt.bar(X_axis - 0.3, accousticness_values, 0.1, label = 'Acousticness') 
+    plt.bar(X_axis - 0.2, danceability_values, 0.1, label = 'Danceability') 
+    plt.bar(X_axis - 0.1, energy_values, 0.1, label = 'Energy') 
+    plt.bar(X_axis + 0, instrumentalness_values, 0.1, label = 'Instrumentalness') 
+    plt.bar(X_axis + 0.1, liveness_values, 0.1, label = 'Liveness') 
+    
+    plt.xticks(X_axis, genres) 
+    plt.xlabel("Groups") 
+    plt.ylabel("Quantifier (0 - 1)") 
+    plt.title("Acoustic Features by Genre") 
+    plt.legend() 
+    plt.show() 
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ connection = pymysql.connect(host='34.130.173.160',
 
 with connection:
     with connection.cursor() as cursor:
-        by_year.songs_released_over_time(cursor)
-        by_year.track_duration_over_time(cursor)
-        genres.genres_per_artist(cursor)
+        # by_year.songs_released_over_time(cursor)
+        # by_year.track_duration_over_time(cursor)
+        # genres.genres_per_artist(cursor)
+        genres.genres_audio_features(cursor)
